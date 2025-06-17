06bf50061c5630f3dedc1e37abe7c82e7a9c60e1
Haswell <maimun.najib@gmedia.id>
2025-06-17 01:47:48 +0000

    2nd commit

--- vpp-restapi-ui-backup/src/api/vpp.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { API_BASE_URL, API_TOKEN } from "./config";
-
-const headers = { Authorization: `Bearer ${API_TOKEN}` };
-
-export const fetchBonds = () => axios.get(`${API_BASE_URL}/bonds`, { headers });
-export const createBond = (data) => axios.post(`${API_BASE_URL}/bonds`, data, { headers });
-// Tambahkan endpoint lain sesuai kebutuhan
