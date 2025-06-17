428b0520bad5016f998e7ef2308d9666eefca54a
Haswell <maimun.najib@gmedia.id>
2025-06-17 01:50:06 +0000

    add interface module

---
+++ src/pages/Interfaces.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Interfaces() {
+  return (
+    <div>
+      <h2>Interfaces</h2>
+      <div>Daftar interfaces akan tampil di sini.</div>
+    </div>
+  );
+}
+
+export default Interfaces;
